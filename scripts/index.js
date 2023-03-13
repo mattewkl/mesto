2d7631692a414f45b21eff9b7b21b5ae50ce0b48
mattewkl <cikle290@gmail.com>
2023-03-13 16:45:22 +0300

    js cards

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,47 @@ let closeButtonObj = document.querySelector('.popup__close-button')
 let formNameObj = document.querySelector('#name')
 let formDescriptionObj = document.querySelector('#description')
 let editFormObj = document.querySelector('.form')
+let cardTemplate = document.querySelector('#card-template').content;
+const gridCards = document.querySelector('.grid-cards')
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+function addCard(link, name) {
+  const firstCard = cardTemplate.querySelector('.grid-cards__item').cloneNode(true);
+  firstCard.querySelector('.grid-cards__item-image').src = link;
+  firstCard.querySelector('.grid-cards__item-image').alt = name;
+  firstCard.querySelector('.grid-cards__caption').textContent = name;
+  gridCards.prepend(firstCard);
+
+}
+
+
+initialCards.forEach(element => addCard(element.link, element.name));
+
 
 function openPopup()  {
   popupObj.classList.add('popup_opened');
@@ -20,7 +61,6 @@ function closePopup() {
 }
 
 function handleFormSubmit(evt) {
-  
   evt.preventDefault();
   nameObj.textContent = formNameObj.value;
   descriptonObj.textContent = formDescriptionObj.value;
@@ -30,3 +70,6 @@ function handleFormSubmit(evt) {
 editButtonObj.addEventListener('click', openPopup)
 closeButtonObj.addEventListener('click', closePopup)
 editFormObj.addEventListener('submit', handleFormSubmit)
+
+console.log('Z'.toLocaleUpperCase())
+console.log('z'.toUpperCase() === 'Z')
