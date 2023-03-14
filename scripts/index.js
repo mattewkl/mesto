d91fd01d074e1fd2ab83768dcaafd936df0b67c0
mattewkl <cikle290@gmail.com>
2023-03-14 07:41:18 +0300

    add card popup realised

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,26 @@ let nameObj = document.querySelector('.profile__name');
 let descriptonObj = document.querySelector('.profile__description');
 let editButtonObj = document.querySelector('.profile__edit-button')
 let popupObj = document.querySelector('.popup')
-let closeButtonObj = document.querySelector('.popup__close-button')
-let formNameObj = document.querySelector('#name')
-let formDescriptionObj = document.querySelector('#description')
-let editFormObj = document.querySelector('.form')
+
+
+const closeEditPopupButton = document.querySelector('.popup__close-button_purpouse_close-edit-profile')
+const closeAddPopupButton = document.querySelector('.popup__close-button_purpouse_close-add-card')
+
+
+let formNameObj = document.querySelector('.form__text-input_purpouse_profile-name')
+let formDescriptionObj = document.querySelector('.form__text-input_purpouse_profile-description')
+
+let formPlaceNameObj = document.querySelector('.form__text-input_purpouse_place-name')
+let formPlaceLinkObj = document.querySelector('.form__text-input_purpouse_place-link')
+
+
+let editFormObj = document.querySelector('.form_purpouse_edit-profile')
+let addCardFormObj = document.querySelector('.form_purpouse_add-card')
 let cardTemplate = document.querySelector('#card-template').content;
+const addButton = document.querySelector('.profile__add-button')
 const gridCards = document.querySelector('.grid-cards')
+const editPopup = document.querySelector('.popup__container_purpouse_edit-profile')
+const addCardPopup = document.querySelector('.popup__container_purpouse_add-card')
 const initialCards = [
   {
     name: 'Архыз',
@@ -35,6 +49,8 @@ const initialCards = [
   }
 ]; 
 
+
+
 function addCard(link, name) {
   const firstCard = cardTemplate.querySelector('.grid-cards__item').cloneNode(true);
   firstCard.querySelector('.grid-cards__item-image').src = link;
@@ -47,29 +63,51 @@ function addCard(link, name) {
 
 initialCards.forEach(element => addCard(element.link, element.name));
 
+function handleAddCardFormSubmit(evt) {
+  evt.preventDefault();
+  addCard(formPlaceLinkObj.value, formPlaceNameObj.value)
+  closePopup();
+}
 
-function openPopup()  {
+function openPopupOverlay()  {
   popupObj.classList.add('popup_opened');
+}
+
+function openAddPopup() {
+  openPopupOverlay();
+  addCardPopup.classList.add('popup__container_opened');
+  
+}
+
+
+function openEditPopup() {
+  openPopupOverlay();
+  editPopup.classList.add('popup__container_opened')
   let nameText = nameObj.textContent;
   let descriptonText = descriptonObj.textContent;
   formNameObj.value = nameText.trim();
   formDescriptionObj.value = descriptonText.trim();
+
 }
+
+
 
 function closePopup() {
   popupObj.classList.remove('popup_opened');
+  editPopup.classList.remove('popup__container_opened');
+  addCardPopup.classList.remove('popup__container_opened');
 }
 
-function handleFormSubmit(evt) {
+function handleEditFormSubmit(evt) {
   evt.preventDefault();
   nameObj.textContent = formNameObj.value;
   descriptonObj.textContent = formDescriptionObj.value;
   closePopup();
 }
 
-editButtonObj.addEventListener('click', openPopup)
-closeButtonObj.addEventListener('click', closePopup)
-editFormObj.addEventListener('submit', handleFormSubmit)
-
-console.log('Z'.toLocaleUpperCase())
-console.log('z'.toUpperCase() === 'Z')
+editButtonObj.addEventListener('click', openEditPopup)
+addButton.addEventListener('click', openAddPopup)
+closeEditPopupButton.addEventListener('click', closePopup)
+closeAddPopupButton.addEventListener('click', closePopup)
+editFormObj.addEventListener('submit', handleEditFormSubmit)
+addCardFormObj.addEventListener('submit', handleAddCardFormSubmit)
