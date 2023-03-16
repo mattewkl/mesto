43e43cffd5a259c56afa18c557bcccc345e5d7d3
mattewkl <cikle290@gmail.com>
2023-03-16 13:25:46 +0300

    fix: review fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,64 @@
 
 
 // editpopup vars
-let nameObj = document.querySelector('.profile__name');
-let descriptonObj = document.querySelector('.profile__description');
-let editButtonObj = document.querySelector('.profile__edit-button')
-const editPopupObj = document.querySelector('.popup_purpouse_edit-profile')
-const closeEditPopupButton = editPopupObj.querySelector('.popup__close-button')
-const formNameObj = document.querySelector('.form__text-input_purpouse_profile-name')
-const formDescriptionObj = document.querySelector('.form__text-input_purpouse_profile-description')
-const editFormObj = document.querySelector('.form_purpouse_edit-profile')
+const nameObj = document.querySelector('.profile__name');
+const descriptonObj = document.querySelector('.profile__description');
+const buttonOpenEditProfile = document.querySelector('.profile__edit-button')
+const popupOpenEditProfile = document.querySelector('.popup_purpouse_edit-profile')
+const buttonCloseEditPopup = popupOpenEditProfile.querySelector('.popup__close-button')
+const formNameObj = document.querySelector('.popup__form-text-input_purpouse_profile-name')
+const formDescriptionObj = document.querySelector('.popup__form-text-input_purpouse_profile-description')
+const formEditObj = document.querySelector('.popup__form_purpouse_edit-profile')
 
 function handleEditFormSubmit(evt) {
   evt.preventDefault();
   nameObj.textContent = formNameObj.value;
   descriptonObj.textContent = formDescriptionObj.value;
-  closePopup(editPopupObj);
+  closePopup(popupOpenEditProfile);
 }
 
 function openEditPopup() {
-  openPopupOverlay(editPopupObj);
-  let nameText = nameObj.textContent;
-  let descriptonText = descriptonObj.textContent;
-  console.log(nameText, descriptonText)
+  openPopupOverlay(popupOpenEditProfile);
+  const nameText = nameObj.textContent;
+  const descriptonText = descriptonObj.textContent;
   formNameObj.value = nameText.trim();
   formDescriptionObj.value = descriptonText.trim();
 }
 
 
+// figurePopup vars
+const figurePopup = document.querySelector('.popup_purpouse_figure')
+const closeFigurePopupButton = figurePopup.querySelector('.popup__close-button')
+const figurePopupImage = figurePopup.querySelector('.popup__figure-popup-image')
+const figurePopupCaption = figurePopup.querySelector('.popup__figure-popup-caption')
+
+function openFigurePopup(name, link) {
+  openPopupOverlay(figurePopup);
+  figurePopupImage.src = link
+  figurePopupImage.alt = name
+  figurePopupCaption.textContent = name
+}      
+
+function openPopupOverlay(popupObj)  {
+  popupObj.classList.add('popup_opened');
+}    
+
+function closePopup(popupObj) {
+  popupObj.classList.remove('popup_opened')  
+}
+
+
+
+
+
 // addcard vars
-let formPlaceNameObj = document.querySelector('.form__text-input_purpouse_place-name')
-let formPlaceLinkObj = document.querySelector('.form__text-input_purpouse_place-link')
-const addCardPopup = document.querySelector('.popup_purpouse_add-card')
-const closeAddPopupButton = addCardPopup.querySelector('.popup__close-button')
-const addCardFormObj = document.querySelector('.form_purpouse_add-card')
-let cardTemplate = document.querySelector('#card-template').content;
-const addButton = document.querySelector('.profile__add-button')
+const formPlaceNameObj = document.querySelector('.popup__form-text-input_purpouse_place-name')
+const formPlaceLinkObj = document.querySelector('.popup__form-text-input_purpouse_place-link')
+const popupAddCard = document.querySelector('.popup_purpouse_add-card')
+const closeAddPopupButton = popupAddCard.querySelector('.popup__close-button')
+const formAddCard = document.querySelector('.popup__form_purpouse_add-card')
+const cardTemplate = document.querySelector('#card-template').content;
+const buttonOpenAddCardPopup = document.querySelector('.profile__add-button')
 const gridCards = document.querySelector('.grid-cards')
 const initialCards = [
   {
@@ -72,59 +96,42 @@ function deleteParentOnClick (event) {
   event.target.parentNode.remove()
 }    
 
-function addCard(link, name) {
+function createCard(link, name) {
   const firstCard = cardTemplate.querySelector('.grid-cards__item').cloneNode(true);
-  const cardImage = firstCard.querySelector('.grid-cards__item-image')
+  const cardImage = firstCard.querySelector('.grid-cards__item-image');
   cardImage.src = link;
   cardImage.alt = name;
   firstCard.querySelector('.grid-cards__caption').textContent = name;
-  gridCards.prepend(firstCard);
   const deleteCardButton = firstCard.querySelector('.grid-cards__delete-btn')
   const likeButton = firstCard.querySelector('.grid-cards__like-button')
-  cardImage.addEventListener('click',() => openFigurePopup(firstCard)); 
+  cardImage.addEventListener('click',() => openFigurePopup(name, link)); 
   likeButton.addEventListener('click',() => toggleLike(likeButton));
   deleteCardButton.addEventListener('click', event => deleteParentOnClick(event) )
+
+  return firstCard
 }    
 
-initialCards.forEach(element => addCard(element.link, element.name));
+initialCards.forEach(element => gridCards.prepend(createCard(element.link, element.name)));
 
 function handleAddCardFormSubmit(evt) {
   evt.preventDefault();
-  addCard(formPlaceLinkObj.value, formPlaceNameObj.value)
-  closePopup(addCardPopup);
+  gridCards.prepend(createCard(formPlaceLinkObj.value, formPlaceNameObj.value));
+  closePopup(popupAddCard);
 }      
 
 function openAddPopup() {
-  openPopupOverlay(addCardPopup);
+  formPlaceLinkObj.value = '';
+  formPlaceNameObj.value = '';
+  openPopupOverlay(popupAddCard);
 }    
 
 
-// figurePopup vars
-const figurePopup = document.querySelector('.popup_purpouse_figure')
-const closeFigurePopupButton = figurePopup.querySelector('.popup__close-button')
-
-function openFigurePopup(card) {
-  openPopupOverlay(figurePopup);
-  const cardImage = card.querySelector('.grid-cards__item-image')
-  figurePopup.classList.add('figure-popup_opened')
-  figurePopup.querySelector('.popup__figure-popup-image').src = cardImage.src
-  figurePopup.querySelector('.popup__figure-popup-image').alt = cardImage.alt
-  figurePopup.querySelector('.popup__figure-popup-caption').textContent = cardImage.alt
-}      
-
-function openPopupOverlay(popupObj)  {
-  popupObj.classList.add('popup_opened');
-}    
-
-function closePopup(popupObj) {
-  popupObj.classList.remove('popup_opened')  
-}
 
 
-editButtonObj.addEventListener('click', openEditPopup)
-addButton.addEventListener('click', openAddPopup)
-closeEditPopupButton.addEventListener('click', () => closePopup(editPopupObj))
-closeAddPopupButton.addEventListener('click', () => closePopup(addCardPopup))
+buttonOpenEditProfile.addEventListener('click', openEditPopup)
+buttonOpenAddCardPopup.addEventListener('click', openAddPopup)
+buttonCloseEditPopup.addEventListener('click', () => closePopup(popupOpenEditProfile))
+closeAddPopupButton.addEventListener('click', () => closePopup(popupAddCard))
 closeFigurePopupButton.addEventListener('click', () => closePopup(figurePopup))
-editFormObj.addEventListener('submit', handleEditFormSubmit)
-addCardFormObj.addEventListener('submit', handleAddCardFormSubmit)
+formEditObj.addEventListener('submit', handleEditFormSubmit)
+formAddCard.addEventListener('submit', handleAddCardFormSubmit)
